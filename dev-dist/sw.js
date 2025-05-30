4437d2edfcee61caf5c76ad05230439020d0e5b0
Felipe Baracaldo <felipe.baracaldo1118@icloud.com>
2025-05-29 21:02:17 -0500

    cambio de las carpetas y funcionalidades

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jut1vidskkg"
+    "revision": "0.aqlttn4c01g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
